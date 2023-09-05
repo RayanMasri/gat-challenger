a4bad6ee9e315d4cf7b6d0bdb10c5e3275fadc35
RayanMasri <35999731+RayanMasri@users.noreply.github.com>
2023-09-05 19:15:19 +0300

    tagged anomaly data.json

--- src/app/anomaly/tagger.py
+++ src/app/anomaly/tagger.py
@@ -5,17 +5,8 @@ with open('./data.json', 'r', encoding='utf8') as file:
 
 total = 0
 for i in range(len(data)):
-    edited = data[i]
-
-    questions = edited["questions"]
-
-    for j in range(len(questions)):
-        questions[j]["id"] = total
-        total += 1
-
-    edited["questions"] = questions
-
-    data[i] = edited
+    data[i]["id"] = total
+    total += 1
 
 with open('./new-data.json', 'a', encoding='utf8') as file:
     file.write(json.dumps(data, ensure_ascii=False))
